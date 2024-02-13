693d209ff6f5efb91b7df9423e9ca72fbe3e6b54
nudsarin12 <miyanonuch@gmail.com>
2024-02-13 14:30:20 +0700

    first commit

---
+++ front/src/layout/RegisterForm.jsx
@@ -0,0 +1,119 @@
+import axios from 'axios';
+import { useState } from "react";
+
+export default function RegisterForm() {
+  const [input, setInput] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    email: ''
+  });
+
+  const hdlChange = e => {
+    setInput(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
+  };
+
+  const hdlSubmit = async e => {
+    try {
+      e.preventDefault();
+      // validation
+      if (input.password !== input.confirmPassword) {
+        return alert('Please check confirm password');
+      }
+      const rs = await axios.post('http://localhost:8889/auth/register', input);
+      console.log(rs);
+      if (rs.status === 200) {
+        alert('Register Successful');
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <div className="p-5 border w-4/6 min-w-[500px] mx-auto rounded mt-5 ">
+      <div className="text-3xl text-center mb-5">สมัครสมาชิก</div>
+
+      <form className="flex flex-col gap-2" onSubmit={hdlSubmit}>
+        <label className="form-control w-full max-w-xs ">
+          <div className="label">
+            <span className="label-text ">First Name</span>
+          </div>
+          <input
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+            name="firstName"
+            value={input.firstName}
+            onChange={hdlChange}
+          />
+        </label>
+        <label className="form-control w-full max-w-xs ">
+          <div className="label">
+            <span className="label-text ">Last Name</span>
+          </div>
+          <input
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+            name="lastName"
+            value={input.lastName}
+            onChange={hdlChange}
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Username</span>
+          </div>
+          <input
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+            name="username"
+            value={input.username}
+            onChange={hdlChange}
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">E-mail</span>
+          </div>
+          <input
+            type="email"
+            className="input input-bordered w-full max-w-xs"
+            name="email"
+            value={input.email}
+            onChange={hdlChange}
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Password</span>
+          </div>
+          <input
+            type="password"
+            className="input input-bordered w-full max-w-xs"
+            name="password"
+            value={input.password}
+            onChange={hdlChange}
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Confirm Password</span>
+          </div>
+          <input
+            type="password"
+            className="input input-bordered w-full max-w-xs"
+            name="confirmPassword"
+            value={input.confirmPassword}
+            onChange={hdlChange}
+          />
+        </label>
+        <div className="flex gap-5 ">
+          <button type="submit" className="btn btn-outline btn-info mt-7">Submit</button>
+          <button type="reset" className="btn btn-outline btn-warning mt-7">Reset</button>
+        </div>
+      </form>
+    </div>
+  );
+}
